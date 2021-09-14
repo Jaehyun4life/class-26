98c4434c37c89e7cb3890ccde85229930d2ebfbc
Yatee <yateejyothidas@gmail.com>
2021-09-14 16:37:25 +0530

    added a platform

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+var platform;
 
 var engine, world;
 var box1, pig1;
@@ -16,7 +17,9 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+
+    platform = new Ground(150,300,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +62,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    platform.display();
 }
